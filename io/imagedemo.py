0058914e1c18834a110bd83d0564cb663e35082c
scott.xie <scott.xie@runmit.com>
2016-08-17 18:21:42 +0800

    image paste

--- io/imagedemo.py
+++ io/imagedemo.py
@@ -9,3 +9,9 @@ cropped_img = img.crop(area)
 
 img.show() #open image by your default application
 cropped_img.show() #open cropped image
+
+bg = Image.open('fo.jpg')
+
+area = (0, 0, 338, 466)
+bg.paste(img, area)
+bg.show()
