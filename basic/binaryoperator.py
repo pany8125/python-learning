dc97881b5ca3ba8a2d4d2f4f3d1fced3fb38f750
scott.xie <scott.xie@runmit.com>
2016-08-19 10:37:34 +0800

    binary operation

---
+++ basic/binaryoperator.py
@@ -0,0 +1,14 @@
+# binary and
+
+a = 25    # 110010
+b = 50    # 011001
+c = a & b # 010000
+
+print(c)
+
+# binary right shift or left shift
+
+x = 240    # 11110000
+y = x >> 2 # 00111100
+z = x << 2 # 1111000000
+print(y, z)
